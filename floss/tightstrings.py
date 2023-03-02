40bf4275906a8b1d558065a6bffc7b31d60e1a13
Moritz <mr-tz@users.noreply.github.com>
2023-03-02 20:11:48 +0100

    Update floss/tightstrings.py

--- floss/tightstrings.py
+++ floss/tightstrings.py
@@ -105,7 +105,7 @@ def extract_tightstrings(
                 for n, ctx in enumerate(ctxs, 1):
                     logger.trace(
                         "extracting tightstring at checkpoint: 0x%x stacksize: 0x%x", ctx.pc, ctx.init_sp - ctx.sp
-                    )                 
+                    )
                     logger.trace("pre_ctx strings: %s", ctx.pre_ctx_strings)
                     for s in extract_strings(ctx.stack_memory, min_length, exclude=ctx.pre_ctx_strings):
                         frame_offset = (ctx.init_sp - ctx.sp) - s.offset - floss.utils.getPointerSize(vw)
