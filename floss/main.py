37ca4fcce3077c3e1c392b6b85d5a93f7dc75019
Arker123 <kharbandarnav@gmail.com>
2023-03-10 00:40:40 +0530

    Updated code style

--- floss/main.py
+++ floss/main.py
@@ -266,8 +266,7 @@ def make_parser(argv):
 
 
 def set_color_config(color):
-    import sys
-    sys.stdout.reconfigure(encoding='utf-8')
+    sys.stdout.reconfigure(encoding="utf-8")
 
     if color == "always":
         colorama.init(strip=False)
