1084f9cee141c265761afd7fe7503d688b36aebd
mohamedadel46 <muhammed195826@feng.bu.edu.eg>
2023-04-29 22:59:26 +0300

    Added Go binary Identification

--- floss/main.py
+++ floss/main.py
@@ -13,6 +13,7 @@ from time import time
 from typing import Set, List, Optional
 
 import halo
+import pefile
 import viv_utils
 import rich.traceback
 import viv_utils.flirt
@@ -257,6 +258,67 @@ def make_parser(argv):
     )
 
     return parser
+
+
+def is_go_bin(sample: str) -> bool:
+    """
+    Check if the binary given is compiled with GO compiler or not
+    it checks the magic header of the pclntab structure -pcHeader-
+    the magic values varies through the version
+    refernce:
+    https://github.com/0xjiayu/go_parser/blob/865359c297257e00165beb1683ef6a679edc2c7f/pclntbl.py#L46
+    """
+    try:
+        pe = pefile.PE(sample)
+    except:
+        logger.debug("NOT valid PE header")
+        return False
+
+    go_magic = [
+        b"\xf0\xff\xff\xff\x00\x00",
+        b"\xfb\xff\xff\xff\x00\x00",
+        b"\xfa\xff\xff\xff\x00\x00",
+        b"\xf1\xff\xff\xff\x00\x00",
+    ]
+
+    # look for the .rdata section first
+    for section in pe.sections:
+        if ".rdata" in section.Name.decode("utf-8").rstrip("\x00"):
+            section_va = section.VirtualAddress
+            section_size = section.SizeOfRawData
+            for magic in go_magic:
+                if magic in section.get_data(section_va, section_size):
+                    pclntab_va = section.get_data(section_va, section_size).index(magic) + section_va
+                    if verify_pclntab(section, pclntab_va):
+                        return True
+            return False
+
+    # if not found, search in all the available sections
+
+    for magic in go_magic:
+        for section in pe.sections:
+            section_va = section.VirtualAddress
+            section_size = section.SizeOfRawData
+            if magic in section.get_data(section_va, section_size):
+                pclntab_va = section.get_data(section_va, section_size).index(magic) + section_va
+                if verify_pclntab(section, pclntab_va):
+                    # just for testing
+                    return True
+    return False
+
+
+def verify_pclntab(section, pclntab_va: int) -> bool:
+    """
+    Parse headers of pclntab to verify it is legit
+    used in go parser itself https://go.dev/src/debug/gosym/pclntab.go
+    """
+    pc_quanum = section.get_data(pclntab_va + 6, 1)
+    pointer_size = section.get_data(pclntab_va + 7, 1)
+    if (pc_quanum != b"\x01" and pc_quanum != b"\x02" and pc_quanum != b"\x04") or (
+        pointer_size != b"\x04" and pointer_size != b"\x08"
+    ):
+        return False
+    return True
 
 
 def set_log_config(debug, quiet):
@@ -527,6 +589,9 @@ def main(argv=None) -> int:
     # 2. stack strings
     # 3. tight strings
     # 4. decoded strings
+
+    if is_go_bin(sample=sample):
+        logger.debug("GO Binary Detected")
 
     if results.analysis.enable_static_strings:
         logger.info("extracting static strings...")
