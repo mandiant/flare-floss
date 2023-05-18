f1b285f9c6b573ab128d13c1f2c62e86e13a0716
mohamedadel46 <muhammed195826@feng.bu.edu.eg>
2023-05-18 22:38:36 +0300

    fix code style

--- tests/test_language_id.py
+++ tests/test_language_id.py
@@ -3,15 +3,17 @@ import pytest
 from floss.language_identifier import is_go_bin
 
 
-@pytest.mark.parametrize("binary_file, expected_result", [
-    ("./tests/data/src/go-hello/bin/go-hello.exe", True),
-    ("./tests/data/src/go-hello/bin/go-hello", False),
-    ("test-decode-to-stack.exe", False),
-    ("./src/shellcode-stackstrings/bin/shellcode-stackstrings.bin", False),
-])
+@pytest.mark.parametrize(
+    "binary_file, expected_result",
+    [
+        ("./tests/data/src/go-hello/bin/go-hello.exe", True),
+        ("./tests/data/src/go-hello/bin/go-hello", False),
+        ("test-decode-to-stack.exe", False),
+        ("./src/shellcode-stackstrings/bin/shellcode-stackstrings.bin", False),
+    ],
+)
 def test_go_binary_detection(binary_file, expected_result):
-
     is_go_binary = is_go_bin(binary_file)
-    
+
     # Check the expected result
     assert is_go_binary == expected_result, f"Expected: {expected_result}, Actual: {is_go_binary}"
