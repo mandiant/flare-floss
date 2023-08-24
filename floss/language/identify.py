c031d5f75909738d9e63a6479544539c4f122044
Arker123 <kharbandarnav@gmail.com>
2023-08-24 17:12:08 +0530

    Add warning for Go binary

--- floss/language/identify.py
+++ floss/language/identify.py
@@ -26,7 +26,9 @@ def identify_language(sample: Path, static_strings: Iterable[StaticString]) -> L
     Identify the language of the binary given
     """
     if is_rust_bin(static_strings):
-        logger.warning("Rust binary detected, FLOSS handles Rust static strings, but string deobfuscation may be inaccurate and take a long time")
+        logger.warning(
+            "Rust binary detected, FLOSS handles Rust static strings, but string deobfuscation may be inaccurate and take a long time"
+        )
         return Language.RUST
 
     # Open the file as PE for further checks
@@ -37,6 +39,9 @@ def identify_language(sample: Path, static_strings: Iterable[StaticString]) -> L
         return Language.UNKNOWN
 
     if is_go_bin(pe):
+        logger.warning(
+            "Go binary detected, FLOSS handles Go static strings, but string deobfuscation may be inaccurate and take a long time"
+        )
         return Language.GO
     elif is_dotnet_bin(pe):
         return Language.DOTNET
