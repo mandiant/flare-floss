b76cf0d0c664bfb077a5178cb7f5e5dddb269060
FredCoast <f.cobb@lancaster.ac.uk>
2025-03-31 13:09:01 +0100

    Updated the JSON output for better usage for QS

--- floss/render/json.py
+++ floss/render/json.py
@@ -23,9 +23,108 @@ class FlossJSONEncoder(json.JSONEncoder):
         return super().default(o)
 
 
+def create_custom_format(doc: ResultDocument):
+    """
+    Format the ResultDocument into a custom JSON structure in the format:
+    Strings: 
+        - static_strings
+        - stack_strings
+        - tight_strings
+        - decoded_strings
+        - Context:
+            - structure
+            - tags
+    File Layout:
+        - file_path
+        - imagebase
+        - min_length
+    """
+    static_strings = []
+    stack_strings = []
+    tight_strings = []
+    decoded_strings = []
+    context = {}
+
+    string_id = 1
+    for string in doc.strings.static_strings:
+        static_strings.append({
+            "id": string_id,
+            "encoding": string.encoding.value.lower(),
+            "offset": string.offset,
+            "length": len(string.string),
+            "string": string.string
+        })
+        
+        if "DOS mode" in string.string:
+            context[string_id] = {
+                "structure": "pe.header",
+                "tags": ["#common"]
+            }
+        elif string.string in ["VirtualQuery", "GetProcAddress", "LoadLibraryA"]:
+            context[string_id] = {
+                "structure": "import table",
+                "tags": ["#winapi", "#common"]
+            }
+        
+        string_id += 1
+
+    for string in doc.strings.stack_strings:
+        stack_entry = {
+            "id": string_id,
+            "encoding": string.encoding.value.lower(),
+            "offset": string.program_counter,
+            "length": len(string.string),
+            "string": string.string,
+            "function": string.function,
+            "stack_pointer": string.stack_pointer,
+            "frame_offset": string.frame_offset
+        }
+        stack_strings.append(stack_entry)
+        string_id += 1
+
+    for string in doc.strings.tight_strings:
+        tight_entry = {
+            "id": string_id,
+            "encoding": string.encoding.value.lower(),
+            "offset": string.program_counter,
+            "length": len(string.string),
+            "string": string.string,
+            "function": string.function,
+            "stack_pointer": string.stack_pointer,
+            "frame_offset": string.frame_offset
+        }
+        tight_strings.append(tight_entry)
+        string_id += 1
+
+    for string in doc.strings.decoded_strings:
+        decoded_strings.append({
+            "id": string_id,
+            "encoding": string.encoding.value.lower(),
+            "offset": string.decoding_routine,
+            "length": len(string.string),
+            "string": string.string,
+            "decoded_at": string.decoded_at,
+            "address": string.address,
+            "address_type": string.address_type
+        })
+        string_id += 1
+
+    return {
+        "strings": {
+            "static_strings": static_strings,
+            "stack_strings": stack_strings,
+            "tight_strings": tight_strings,
+            "decoded_strings": decoded_strings,
+            "context": context
+        },
+        "file_layout": {
+            "file_path": doc.metadata.file_path,
+            "imagebase": doc.metadata.imagebase,
+            "min_length": doc.metadata.min_length
+        }
+    }
+
+
 def render(doc: ResultDocument) -> str:
-    return json.dumps(
-        doc,
-        cls=FlossJSONEncoder,
-        sort_keys=True,
-    )
+    custom_format = create_custom_format(doc)
+    return json.dumps(custom_format, indent=4)
